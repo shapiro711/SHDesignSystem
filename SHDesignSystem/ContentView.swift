48b9f2ef4db1cee5b7cb56b882c737c6cf314feb
rladbgus <cocokiuuu1858@gmail.com>
2026-01-08 19:34:19 +0900

    Initial Commit

---
+++ SHDesignSystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SHDesignSystem
+//
+//  Created by Kim Do hyung on 1/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SHDesignSystem/SHDesignSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SHDesignSystemApp.swift
+//  SHDesignSystem
+//
+//  Created by Kim Do hyung on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct SHDesignSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
